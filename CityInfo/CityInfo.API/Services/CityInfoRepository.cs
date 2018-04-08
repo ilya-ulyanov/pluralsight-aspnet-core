54d189715e3aab2e87909584e16697e6f085867f
ILLIA ULIANOV <ulyanov.iv@gmail.com>
2018-04-08 17:14:17 +0200

    introducing CityInfoRepository + registering it as a service for DI

---
+++ CityInfo/CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext cityInfoContext;
+
+        public CityInfoRepository(CityInfoContext cityInfoContext)
+        {
+            this.cityInfoContext = cityInfoContext;
+        }
+
+        public IEnumerable<City> GetCities()
+        {
+            return this.cityInfoContext.Cities.ToList();
+        }
+
+        public City GetCity(int cityId, bool includePointsOfInterest)
+        {
+            IQueryable<City> query = this.cityInfoContext.Cities;
+            if (includePointsOfInterest)
+            {
+                query.Include(c => c.PointsOfInterest);
+            }
+
+            return query.SingleOrDefault(c => c.Id == cityId);
+        }
+
+        public PointOfInterest GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            return this.cityInfoContext.PointsOfInterest.SingleOrDefault(poi => poi.CityId == cityId && poi.Id == pointOfInterestId);
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId)
+        {
+            return this.cityInfoContext.PointsOfInterest.Where(poi => poi.CityId == cityId).ToList();
+        }
+    }
+}
---
+++ CityInfo/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.API.Entities;
+using System.Collections.Generic;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+
+        City GetCity(int cityId, bool includePointsOfInterest);
+
+        IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId);
+
+        PointOfInterest GetPointOfInterest(int cityId, int pointOfInterestId);
+    }
+}
